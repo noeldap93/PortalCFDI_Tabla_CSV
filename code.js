48562eb74aac8ebefbf2faf7070dd5a7498972f7
Jose Noel Martin Del Angel Polanco <aragonknoel@gmail.com>
2017-12-31 12:19:44 -0600

    Se agregaron ID's pendientes y Se realizaron correcciones. $().find() no retornaba un arreglo si no un objeto-arreglo de jQuery. La funcion map de este  se utiliza diferente.

--- code.js
+++ code.js
@@ -1,20 +1,22 @@
-let monthCtrl = $("#");
-let yearCtrl = $("#");
+(function () {
 
-function load(){
-    throw "ids must be set..."
-    let tbl = $("#");
-    let csv = tbl.find("tr").map(function(trow) {
-        return $(trow).find("td").map(function(tcell){
-            return tcell.textContent; // obtenemos el texto de cada celda
-        }).join(","); // unimos todas las celdas de la fila con comas
-    }).join("\n"); // unimos las filas de la tabla por saltos de linea
-    console.log("result:", csv);
-    let storageName = getStorageName(ano_ctrl.value(),mes_ctrl.value())
-    localStorage.setItem(storageName, csv);
+    let monthCtrl = $("#ctl00_MainContent_CldFecha_DdlMes");
+    let yearCtrl = $("#DdlAnio");
 
-}
+    function load() {
+        let tbl = $("#ctl00_MainContent_tblResult");
+        let csv = tbl.find("tr").map(function (i, trow) {
+            return $(trow).find("td").map(function (j, tcell) { // map no es el nativo de array, es un obj-array jQuery.
+                return tcell.textContent; // obtenemos el texto de cada celda
+            }).get().join(','); // unimos todas las celdas de la fila con comas
+        }).get().join('\n'); // unimos las filas de la tabla por saltos de linea
+        console.log("result:", csv);
+        let storageName = getStorageName(yearCtrl.val(), monthCtrl.val())
+        localStorage.setItem(storageName, csv);
+    }
 
-function getStorageName(year,month){
-    return `cfdi_data[${year}_${month}]`;
-}
+    function getStorageName(year, month) {
+        return `cfdi_data[${year}_${month}]`;
+    }
+    return { load, getStorageName };
+})();
